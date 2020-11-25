b292671695de0b52aa3fc0cb7a9a0784cab4c137
Eudaldz <eudald.elias@gmail.com>
2020-11-25 18:38:29 +0100

    Damage calculation

---
+++ Assets/Scripts/DamageController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class DamageController : MonoBehaviour
+{
+
+    public float impactCoefficient = 1f;
+    public float slashCoefficient = 2f;
+    public float impactThreshold = 15f;
+    public float slashThreshold = 15f;
+    public float damageThreshold = 1f;
+
+    public float stunRatio = 0.04f;
+    public float stunThreshold = 0.5f;
+
+    public Color damageDebugColor = Color.red;
+    public Color stunDebugColor = Color.blue;
+
+    private const float debugLinesDuration = 0.2f;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        applyDamageForces(col);
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        applyDamageForces(col);
+    }
+
+    private void applyDamageForces(Collision2D col)
+    {
+        float impactForce;
+        float slashForce;
+        Vector2 point;
+        Vector2 normal;
+        if(col.contactCount == 1)
+        {
+            ContactPoint2D cp = col.GetContact(0);
+            impactForce = cp.normalImpulse * impactCoefficient;
+            slashForce = cp.tangentImpulse * slashCoefficient;
+            point = cp.point;
+            normal = cp.normal;
+        }
+        else
+        {
+            ContactPoint2D cp1 = col.GetContact(0);
+            ContactPoint2D cp2 = col.GetContact(1);
+            impactForce = (cp1.normalImpulse + cp2.normalImpulse) * impactCoefficient;
+            slashForce = (cp2.tangentImpulse + cp2.tangentImpulse) * slashCoefficient;
+            point = (cp1.point + cp2.point) / 2f;
+            normal = cp1.normal;
+        }
+        //Debug.DrawRay(point, normal * impactForce * 0.01f, damageDebugColor, debugLinesDuration, false);
+        //Debug.DrawRay(point, Vector2.Perpendicular(normal) * slashForce * 0.01f, damageDebugColor, debugLinesDuration, false);
+
+        float impactDamage = Mathf.Max(impactForce - impactThreshold, 0);
+        float slashDamage = Mathf.Max(slashForce - slashThreshold, 0);
+        float damage = impactDamage + slashDamage;
+        if (damage > damageThreshold)
+        {
+            Debug.Log(this.gameObject.name + " damage: " + damage);
+            //TODO: send damage to object controller
+        }
+
+        float stun = impactDamage * stunRatio;
+        if(stun > stunThreshold)
+        {
+            Debug.Log(this.gameObject.name + " stun: " + stun);
+            //TODO: send stun to object controller
+        }
+
+    }
+}
---
+++ Assets/Scripts/MonkController.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkController : MonoBehaviour
+{
+    [SerializeField]
+    private float health;
+
+    [SerializeField]
+    private float runSpeed;
+    [SerializeField]
+    private float runForce;
+
+    [SerializeField]
+    private float glideSpeed;
+    [SerializeField]
+    private float glideForce;
+
+    [SerializeField]
+    private float groundTurningForce;
+    [SerializeField]
+    private float airTurningForce;
+
+    [SerializeField]
+    private float armExtendingForce;
+    [SerializeField]
+    private float armRotatingForce;
+
+    //internal states
+    private int sustainability;
+    public const int AIRBORNE = 0;
+    public const int GROUNDED = 1;
+    public const int SLIDING = 2;
+
+    private int stability;
+    public const int STABLE = 0;
+    public const int RECOVERING = 2;
+    public const int STUNNED = 1;
+
+    private int stance;
+    public const int OFFENSIVE = 0;
+    public const int DEFENSIVE = 1;
+
+    private int formation;
+    public const int STRAIGHT = 0;
+    public const int DIVING = 1;
+
+
+    //internal variables
+    private float currentHealth;
+    private float knockDuration;
+
+
+    //monk actions
+    float horizontalAxis;
+    float verticalAxis;
+    bool jumpAction;
+    bool diveAction;
+    bool defenseAction;
+
+    //internal controllers
+    Rigidbody2D rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //default starting states
+        sustainability = AIRBORNE;
+        stability = STABLE;
+        stance = OFFENSIVE;
+        formation = STRAIGHT;
+        //init controllers
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //readActionInput();
+        if (rb.IsSleeping())
+        {
+            //Debug.Log("Sleeping");
+        }
+    }
+
+    private void readActionInput()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
+        jumpAction = Input.GetKey("w");
+        diveAction = Input.GetKey("space");
+        defenseAction = Input.GetMouseButtonDown(0);
+
+    }
+
+    private void recieveImpactKnocks()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        calculateImpactDamage(col);
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        //calculateImpactDamage(col);
+    }
+
+    private float calculateImpactDamage(Collision2D col)
+    {
+        float result = 0.0f;
+        const float MIN_DISTANCE = 0.1f;
+        const float NORMAL_COEFF = 1f/10;
+        const float TANGENT_COEFF = 1f/100;
+        const float DAMAGE_THRESHOLD = 5;
+        float normalPressure = 0;
+        float tangentPressure = 0;
+        if (col.contactCount == 1)
+        {
+            
+            ContactPoint2D cp = col.GetContact(0);
+            normalPressure = cp.normalImpulse / MIN_DISTANCE;
+            tangentPressure = cp.tangentImpulse / MIN_DISTANCE;
+        }
+        else if(col.contactCount == 2)
+        {
+            ContactPoint2D cp1 = col.GetContact(0);
+            ContactPoint2D cp2 = col.GetContact(1);
+            float surfaceArea = Mathf.Max(Vector2.Distance(cp1.point, cp2.point), MIN_DISTANCE);
+
+            normalPressure = (cp1.normalImpulse + cp2.normalImpulse) / surfaceArea;
+            tangentPressure = (cp1.tangentImpulse + cp2.tangentImpulse) / surfaceArea;
+        }
+
+        Debug.Log("Normal Pressure: " + normalPressure + " Tangent Pressure: " + tangentPressure);
+
+        float damage = normalPressure * NORMAL_COEFF + tangentPressure * TANGENT_COEFF - DAMAGE_THRESHOLD;
+        Debug.Log("Damage: " + damage);
+        damage = damage > 0 ? damage : 0;
+        return 0;
+    }
+
+    private void impactDemo(Collision2D col)
+    {
+        Debug.Log(col.gameObject.name);
+        foreach(ContactPoint2D cp in col.contacts)
+        {
+            float vel = Vector2.SqrMagnitude(cp.relativeVelocity);
+            float impulse = cp.normalImpulse;
+            Debug.Log("vel: " + vel + "impulse: " + impulse);
+        }
+    }
+
+    private float calculateImpactStun(Collision2D col)
+    {
+        float result = 0.0f;
+
+        return result;
+    }
+}
