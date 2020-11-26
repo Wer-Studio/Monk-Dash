696086957c0a4e31be9f3e22ed66a6f31679304b
Eudaldz <eudald.elias@gmail.com>
2020-11-26 18:28:54 +0100

    Polearm demo

---
+++ Assets/Scripts/PolearmController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolearmController : MonoBehaviour
+{
+
+    public RelativeJoint2D polearmJoint;
+
+    public float maxDistance = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 targetVector = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //Debug.Log(targetVector);
+        Vector2.ClampMagnitude(targetVector, maxDistance);
+        polearmJoint.linearOffset = Vector2.ClampMagnitude(targetVector, maxDistance);
+        float targetAngle = Vector2.SignedAngle(Vector2.right, targetVector);
+        if (targetAngle < 0) targetAngle += 360;
+        float currentAngle = polearmJoint.angularOffset % 360;
+        if (currentAngle < 0) currentAngle += 360;
+        float offset = targetAngle - currentAngle;
+        if (offset > 180) offset -= 360;
+        if (offset < -180) offset += 360;
+        Debug.Log(currentAngle);
+        Debug.Log(offset);
+        polearmJoint.angularOffset += offset;
+    }
+}
