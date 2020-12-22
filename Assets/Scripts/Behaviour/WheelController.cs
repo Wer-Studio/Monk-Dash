9dc80bc7b0b15d42932d92eecc6420ce03c265c8
Eudaldz <eudald.elias@gmail.com>
2020-12-22 14:52:27 +0100

    Leg Movement

--- Assets/Scripts/Behaviour/WheelController.cs
+++ Assets/Scripts/Behaviour/WheelController.cs
@@ -7,99 +7,79 @@ public class WheelController : MonoBehaviour
     public HingeJoint2D legHingeJoint;
     public SliderJoint2D legSliderJoint;
 
-    [Header("Joint Settings")]
+    [Header("Vertical Movement")]
     public float maxExtensionForce;
-    public float jumpSpeed;                 //Speed when jumping.
-    public float extensionSpeed;            //Speed when extending.
-    public float uprightExtensionSelector;  // 0 CROUCHED | 0.5 UPRIGHT (DEFAULT) | 1 EXTENDED (MAX JUMP POS).
-    public float hSpeed;
+    public float extensionJumpSpeed;         //Speed when jumping.
+    public float extensionMoveSpeed;    //Speed when extending.
+    public float restingExtensionSelector;  //0 CROUCHED | 0.5 UPRIGHT (DEFAULT) | 1 EXTENDED (MAX JUMP POS).
 
+    [Header("Horizontal Movement")]
+    public float maxTranslationForce;
+    public float translationMoveSpeed;
 
-    [Header("Control Settings")]
     //..................................
 
     //Private States
     private JointMotor2D legHinge = new JointMotor2D();
     private JointMotor2D legSlider = new JointMotor2D();
+    //.........................................................
 
     //Private Utility Objects
-    
-    //.........................................................
+    KeyProcessor kp = new KeyProcessor();
+    //................................
+
 
     // Start is called before the first frame update
     void Start()
     {
-        legHingeJoint.motor = legHinge;
-        legSliderJoint.motor = legSlider;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
     }
 
     void FixedUpdate()
     {
-        //Get movement delta
-        float hDelta = 0;
-        float sliderSelector = uprightExtensionSelector;// 0 CROUCHED | 0.5 UPRIGHT (DEFAULT) | 1 EXTENDED (MAX JUMP POS)
-        float sliderSpeed = extensionSpeed;
+        //Get input
+        int hAction = kp.HorizontalAction;
+        int vAction = kp.VerticalAction;
+        int jumpAction = kp.JumpAction;
 
-        //Vertical exclusive switch
-        if (Input.GetKey("w") && !Input.GetKey("s")) // CASE MONK HAS TO EXTEND.
-        {
-            print("w");
-            sliderSelector = uprightExtensionSelector;
-            sliderSpeed = extensionSpeed;
-        }
-        if (Input.GetKey("s") && !Input.GetKey("w")) // CASE MONK HAS TO CROUCH DOWN.
-        {
-            print("s");
-            sliderSelector = 0;
-            sliderSpeed = extensionSpeed;
+        float extensionSelector = restingExtensionSelector;
+        float extensionSpeed = extensionMoveSpeed;
+        float translationVelocity = hAction * translationMoveSpeed;
+
+        if(vAction == -1) {
+            extensionSelector = 0;
+        }else if(vAction == 1) {
+            extensionSelector = 1;
         }
 
-        //Horitzontal exclusive switch
-        if (Input.GetKey("a") && !Input.GetKey("d")) // CASE MONK HAS TO GO RIGHT. 
-        {
-            print("a");
-            hDelta = -hSpeed;
-        }
-        if (Input.GetKey("d") && !Input.GetKey("a")) // CASE MONK HAS TO GO LEFT.
-        {
-            print("d");
-            hDelta = hSpeed;
+        if(jumpAction == 1) {
+            extensionSelector = 1;
+            extensionSpeed = extensionJumpSpeed;
         }
 
-        //Jump. Overrides W and S.
-        if (Input.GetKey(KeyCode.Space))    // Jumping conditional is independent from other inputs.
-        {
-            print("JUMP!");
-            sliderSelector = 1;
-            sliderSpeed = jumpSpeed;
-        }
-
-        //MovelegHinge(hDelta);
-        MovelegSlider(sliderSelector, sliderSpeed);
+        MoveLegHinge(translationVelocity);
+        MoveLegSlider(extensionSelector, extensionSpeed);
     }
 
-    private void MovelegHinge(float hDelta)
+    private void MoveLegHinge(float translationVelocity)
     {
-       
+        Debug.Log("Translation: " + translationVelocity);
+        legHinge.maxMotorTorque = maxTranslationForce;
+        legHinge.motorSpeed = - Mathf.Rad2Deg * translationVelocity;
+        legHingeJoint.motor = legHinge;
     }
 
     // This function decides the position of the vertical slider.
     // Semantically it decides how crouched or extended the monk's back is (so jumping has more inertia and looks more real)
-    private void MovelegSlider(float sliderSelector, float sliderVelocity)
+    private void MoveLegSlider(float extensionSelector, float maxExtensionSpeed)
     {
-        float limMin= legSliderJoint.limits.min;
+        float limMin = legSliderJoint.limits.min;
         float limMax = legSliderJoint.limits.max;
-        float targetExtension = Mathf.Lerp(limMin, limMax, sliderSelector);
+        float targetExtension = Mathf.Lerp(limMin, limMax, extensionSelector);
         float currentExtension = legSliderJoint.jointTranslation;
-        float currentSpeed = legSliderJoint.jointSpeed;
-        float deltaExtension =  + targetExtension - currentExtension - currentSpeed*Time.fixedDeltaTime; //TODO Correct bouncing
-
-        legSlider.motorSpeed = Mathf.Clamp(deltaExtension/Time.fixedDeltaTime, -sliderVelocity, sliderVelocity);
+        float deltaExtension =  targetExtension - currentExtension;
+        //Debug.Log("Speed: " + currentSpeed + "Delta Ext: " + deltaExtension);
+        legSlider.motorSpeed = Mathf.Clamp(deltaExtension / Time.fixedDeltaTime, -maxExtensionSpeed, maxExtensionSpeed);
         legSlider.maxMotorTorque = maxExtensionForce;
         legSliderJoint.motor = legSlider;
     }
---
+++ Assets/Scripts/Model/KeyProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyProcessor
+{
+    public int HorizontalAction { get { 
+            if(Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) return -1;
+            if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)) return 1;
+            return 0;
+        } }
+    
+    public int VerticalAction { get {
+            if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) return 1;
+            if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W)) return -1;
+            return 0;
+        } }
+    
+    public int JumpAction { get {
+            if (Input.GetKey(KeyCode.Space)) return 1;
+            return 0;
+        } }
+
+}
+
